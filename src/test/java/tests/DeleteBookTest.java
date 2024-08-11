02267290f54b05732daabefb3ae316a479f06760
Dmitry Kostjuchenko <kdp.029029@gmail.com>
2024-08-11 22:59:05 +0300

    add cookies

--- src/test/java/tests/DeleteBookTest.java
+++ src/test/java/tests/DeleteBookTest.java
@@ -1,9 +1,14 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import io.restassured.response.Response;
 import models.AuthModel;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
 import static specs.DeleteBookSpec.loginRequestSpec;
 import static specs.DeleteBookSpec.loginResponseSpec;
@@ -15,13 +20,18 @@ public class DeleteBookTest extends BaseTest {
         AuthModel model = new AuthModel(getUserName(), getPassword());
         Response authResponse = given(loginRequestSpec)
                 .body(model)
-
                 .when()
                 .post("Account/v1/Login")
-
                 .then()
                 .spec(loginResponseSpec)
                 .extract().response();
-    }
 
+        open("/favicon.ico");
+        getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("userId")));
+        getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
+        getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
+
+        open("/profile");
+        $("#userName-value").should(Condition.text(getUserName()));
+    }
 }
